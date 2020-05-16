1995132ae8f1943291dbec66450ec8e2712da012
Jamie Kirkwin <jkirkwin@gmail.com>
2020-05-15 22:24:12 -0700

    Add binary field

---
+++ uvcompress/binary_field.cpp
@@ -0,0 +1,40 @@
+#include "binary_field.h"
+#include <cassert>
+
+void BinaryField::reverse() {
+    int mid = bits/2;
+    int i;
+    for (i = 0; i < mid; i++) {
+        swapBits(i, bits - i);
+    }
+}
+
+bool BinaryField::isBitSet(int i) {
+    assert (0 <= i && i <= bits);
+    return (data >> i) & (1);
+}
+
+void BinaryField::setBit(int i) {
+    assert (0 <= i && i <= bits);
+    
+    // Use of 1U influenced by http://graphics.stanford.edu/~seander/bithacks.html#SwappingBitsXOR
+    data = data | (1U << i);
+}
+
+void BinaryField::unsetBit(int i) {
+    assert (0 <= i && i <= bits);
+    
+    // Use of 1U influenced by http://graphics.stanford.edu/~seander/bithacks.html#SwappingBitsXOR
+    unsigned short mask = ~(1U << i);
+    data = data & mask;
+}
+
+void BinaryField::swapBits(int i, int j) {
+    assert (validBitCount(i) && validBitCount(j)); 
+    if (isBitSet(i) == isBitSet(j)) {
+        return;
+    } else {
+        flipBit(i);
+        flipBit(j);
+    }
+}
---
+++ uvcompress/binary_field.h
@@ -0,0 +1,78 @@
+#ifndef BIN_FIELD_H
+#define BIN_FIELD_H
+
+#include <cassert>
+
+#define MAX_BITS 16
+
+/*
+ * Represents a sequence of between 0 and 17 bits.
+ */ 
+class BinaryField {
+    public:
+        BinaryField(unsigned short data, int bits) {
+            assert (validBitCount(bits));
+            this->bits = bits;
+            this->data = data;
+        }
+  
+        int getBits() const {
+            return bits;
+        }
+  
+        int getData() {
+            return data;
+        }
+
+        void reverse();
+
+        /*
+         * Answers whether the ith bit is set (where the lsbit is indexed 0).
+         */ 
+        bool isBitSet(int i);
+
+        /*
+         * Sets the ith bit (where the lsbit is indexed 0).
+         */ 
+        void setBit(int i); 
+        
+        /*
+         * Clears the ith bit (where the lsbit is indexed 0).
+         */ 
+        void unsetBit(int i);
+
+        /*
+         * Clears the ith bit if it is set. Otherwise sets the ith bit (where 
+         * the lsbit is indexed 0).
+         */ 
+        void flipBit(int i) {
+            if (isBitSet(i)) {
+                unsetBit(i);
+            } else {
+                setBit(i);
+            }
+        }
+
+        /*
+         * Sets or clears the ith bit (where the lsbit is indexed 0).
+         */ 
+        void setBitTo(int i, bool val) {
+            if (val) {
+                setBit(i);
+            } else {
+                unsetBit(i);
+            }
+        }
+  
+    private:
+        unsigned short data;
+        int bits;
+
+        bool validBitCount(int i) {
+            return 0 <= i && i <= MAX_BITS;
+        }
+
+        void swapBits(int i, int j); 
+};
+
+#endif
