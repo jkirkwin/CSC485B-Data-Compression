99801bb857254228108fc35bb74530dfda9d874e
Jamie Kirkwin <jkirkwin@gmail.com>
2020-06-07 12:12:02 -0700

    Add boost

--- uvgz/binary.h
+++ uvgz/binary.h
@@ -6,7 +6,10 @@
 #define UVGZ_BINARY_H
 
 #include <cstdint>
+#include <boost/dynamic_bitset.hpp>
 
+// Aliases for readability
+using bitset = boost::dynamic_bitset<>;
 using u8 = std::uint8_t;
 using u16 = std::uint16_t;
 using u32 = std::uint32_t;
