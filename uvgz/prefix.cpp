77dbd025c4ac04040cfc5db0e94bd1a300580324
Jamie Kirkwin <jkirkwin@gmail.com>
2020-06-16 21:58:20 -0700

    Convert prefix.h to a static library

---
+++ uvgz/prefix.cpp
@@ -0,0 +1,70 @@
+#include "prefix.h"
+
+std::vector<u32> getFixedLLCodeLengths() {
+    std::vector<u32> lengths {};
+
+    const auto entryCount = fixedLLCodeLengths.size();
+    const auto lastEntry = fixedLLCodeLengths.at(entryCount - 1);
+    const auto totalLengths = lastEntry.at(1) + 1;
+    lengths.reserve(totalLengths);
+
+    for (auto range : fixedLLCodeLengths) {
+        const auto rangeStart {range.at(0)};
+        const auto rangeEnd {range.at(1)};
+        const auto bits {range.at(2)};
+        for(int i = rangeStart; i <= rangeEnd; i++) {
+            lengths.push_back(bits);
+        }
+    }
+    return lengths;
+}
+
+std::vector<u32> getFixedDistanceCodeLengths() {
+    // 32 lengths at 5 bits each
+    std::vector<u32> lengths {};
+    lengths.reserve(32);
+    const u32 bits = 5;
+
+    for(int i = 0; i < 32; i++) {
+        lengths.push_back(bits);
+    }
+    return lengths;
+}
+
+// Minimally adapted from the block2 starter code provided by Bill.
+std::vector<bitset> constructCanonicalCode(std::vector<u32> const & lengths){
+
+    unsigned int size = lengths.size();
+    std::vector< unsigned int > length_counts(16,0); //Lengths must be less than 16 for DEFLATE
+    u32 max_length = 0;
+    for(auto i: lengths){
+        assert(i <= 15);
+        length_counts.at(i)++;
+        max_length = std::max(i, max_length);
+    }
+    length_counts.at(0) = 0; //Disregard any codes with alleged zero length
+
+    std::vector< bitset > result_codes(size);
+
+    //The algorithm below follows the pseudocode in RFC 1951
+    std::vector< unsigned int > next_code(size,0);
+    {
+        //Step 1: Determine the first code for each length
+        unsigned int code = 0;
+        for(unsigned int i = 1; i <= max_length; i++){
+            code = (code+length_counts.at(i-1))<<1;
+            next_code.at(i) = code;
+        }
+    }
+    {
+        //Step 2: Assign the code for each symbol, with codes of the same length being
+        //        consecutive and ordered lexicographically by the symbol to which they are assigned.
+        for(unsigned int symbol = 0; symbol < size; symbol++){
+            unsigned int length = lengths.at(symbol);
+            if (length > 0) {
+                result_codes.at(symbol) = bitset(length, next_code.at(length)++);
+            }
+        }
+    }
+    return result_codes;
+}
--- uvgz/prefix.h
+++ uvgz/prefix.h
@@ -21,40 +21,13 @@ constexpr std::array<std::array<u32, 3>, 4> fixedLLCodeLengths {{
  * To be used with constructCanonicalCode()
  * @return The lengths used for the fixed block type 1 LL code.
  */
-std::vector<u32> getFixedLLCodeLengths() {
-    std::vector<u32> lengths {};
-
-    const auto entryCount = fixedLLCodeLengths.size();
-    const auto lastEntry = fixedLLCodeLengths.at(entryCount - 1);
-    const auto totalLengths = lastEntry.at(1) + 1;
-    lengths.reserve(totalLengths);
-
-    for (auto range : fixedLLCodeLengths) {
-        const auto rangeStart {range.at(0)};
-        const auto rangeEnd {range.at(1)};
-        const auto bits {range.at(2)};
-        for(int i = rangeStart; i <= rangeEnd; i++) {
-            lengths.push_back(bits);
-        }
-    }
-    return lengths;
-}
+std::vector<u32> getFixedLLCodeLengths();
 
 /**
  * To be used with constructCanonicalCode()
  * @return The lengths used for the fixed block type 1 Distance code.
  */
-std::vector<u32> getFixedDistanceCodeLengths() {
-    // 32 lengths at 5 bits each
-    std::vector<u32> lengths {};
-    lengths.reserve(32);
-    const u32 bits = 5;
-
-    for(int i = 0; i < 32; i++) {
-        lengths.push_back(bits);
-    }
-    return lengths;
-}
+std::vector<u32> getFixedDistanceCodeLengths();
 
 
 /** Adapted from Bill's provided code.
@@ -64,48 +37,13 @@ std::vector<u32> getFixedDistanceCodeLengths() {
  *
  * The codes for symbols with length zero are undefined.
  */
-std::vector< bitset > constructCanonicalCode( std::vector<u32> const & lengths ){
-
-    unsigned int size = lengths.size();
-    std::vector< unsigned int > length_counts(16,0); //Lengths must be less than 16 for DEFLATE
-    u32 max_length = 0;
-    for(auto i: lengths){
-        assert(i <= 15);
-        length_counts.at(i)++;
-        max_length = std::max(i, max_length);
-    }
-    length_counts.at(0) = 0; //Disregard any codes with alleged zero length
-
-    std::vector< bitset > result_codes(size);
-
-    //The algorithm below follows the pseudocode in RFC 1951
-    std::vector< unsigned int > next_code(size,0);
-    {
-        //Step 1: Determine the first code for each length
-        unsigned int code = 0;
-        for(unsigned int i = 1; i <= max_length; i++){
-            code = (code+length_counts.at(i-1))<<1;
-            next_code.at(i) = code;
-        }
-    }
-    {
-        //Step 2: Assign the code for each symbol, with codes of the same length being
-        //        consecutive and ordered lexicographically by the symbol to which they are assigned.
-        for(unsigned int symbol = 0; symbol < size; symbol++){
-            unsigned int length = lengths.at(symbol);
-            if (length > 0) {
-                result_codes.at(symbol) = bitset(length, next_code.at(length)++);
-            }
-        }
-    }
-    return result_codes;
-}
+std::vector<bitset> constructCanonicalCode(std::vector<u32> const & lengths);
 
 /**
  * @return A vector of code words for the fixed type 1 LL code in the format
  * specified by constructCanonicalCode().
  */
-std::vector<bitset> getFixedLLCode() {
+inline std::vector<bitset> getFixedLLCode() {
     return constructCanonicalCode(getFixedLLCodeLengths());
 }
 
@@ -113,12 +51,12 @@ std::vector<bitset> getFixedLLCode() {
  * @return A vector of code words for the fixed type 1 distance code in the
  * format specified by constructCanonicalCode().
  */
-std::vector<bitset> getFixedDistanceCode() {
+inline std::vector<bitset> getFixedDistanceCode() {
     return constructCanonicalCode(getFixedDistanceCodeLengths());
 }
 
-// todo - type 2 - CL code specifics
 // todo - type 2 - generate dynamic codes
+// todo - type 2 - CL code specifics
 // todo - optimization - package-merge implementation
 
 #endif //UVGZ_PREFIX_H
