3b802414851af6aff5abad1eef44f1098e2e9118
Jamie Kirkwin <jkirkwin@gmail.com>
2020-07-10 22:11:42 -0700

    Add constants.h

---
+++ uvzz/include/constants.h
@@ -0,0 +1,7 @@
+#ifndef UVZZ_CONSTANTS_H
+#define UVZZ_CONSTANTS_H
+
+const u16 FILE_TYPE_MAGIC_NUMBER = 0x4A4B; // Ascii 'JK'
+const u32 BLOCK_SIZE = 1u << 10u; // 1kb todo increase this
+
+#endif //UVZZ_CONSTANTS_H
