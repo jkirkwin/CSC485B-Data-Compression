dca1cef8cf158338306010f1e61fccc7063e9534
Jamie Kirkwin <jkirkwin@gmail.com>
2020-07-11 02:43:22 -0700

    Add trivial comment

--- uvzz/include/constants.h
+++ uvzz/include/constants.h
@@ -2,6 +2,6 @@
 #define UVZZ_CONSTANTS_H
 
 const u16 FILE_TYPE_MAGIC_NUMBER = 0x4A4B; // Ascii 'JK'
-const u32 BLOCK_SIZE = 100u << 10u;
+const u32 BLOCK_SIZE = 100u << 10u; // 100 KB
 
 #endif //UVZZ_CONSTANTS_H
