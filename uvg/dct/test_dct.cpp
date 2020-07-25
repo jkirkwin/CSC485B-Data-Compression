6f4ea4646c181eb7d31ed89923d1d49c3048fa70
Jamie Kirkwin <jkirkwin@gmail.com>
2020-07-25 00:26:45 -0700

    Remove garbage from dct test file and add todo

--- uvg/dct/test_dct.cpp
+++ uvg/dct/test_dct.cpp
@@ -1,59 +1,9 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
-#include "dct.h"
 #include "matrix.h"
 
-using namespace dct;
-
-//template<typename T>
-//std::string matrixToString(Eigen::MatrixX<T> m) {
-//    std::stringstream ss;
-//    ss << m;
-//    return ss.str();
-//}
-
-//TEST_CASE("Check DC coefficient", "") {
-//    quantize::quantizer_t quantizer;
-//    quantizer.setConstant(100000);
-//    quantizer(0, 0) = 1; // Keep only the DC coefficient
-//
-//    auto inputMatrix = raw_block_t::Ones();
-//    auto encoded = transform(inputMatrix, quantizer);
-//
-//    REQUIRE(encoded.size() == 1);
-//    auto block = encoded.at(0);
-//    REQUIRE(block.size() == BLOCK_CAPACITY);
-//
-//    // todo compute what the DC coefficient should be
-////    REQUIRE(block.at(0) == 1);
-//    for (int i = 1; i < BLOCK_CAPACITY; ++i) {
-//        REQUIRE(block.at(i) == 0);
-//    }
-//}
-
-//TEST_CASE("No quantization yields rounded values", "") {
-//    raw_block_t input;
-//    input.setConstant(100);
-//
-//    auto dctResult = transform(input, quantize::none());
-//    inversionContext ctx = {BLOCK_DIMENSION, BLOCK_DIMENSION, quantize::none()};
-//    auto decoded = invert(dctResult, ctx);
-//
-//    // todo check that decoded is at least close to the input
-//    for (int i = 0; i < BLOCK_DIMENSION; ++i) {
-//        for (int j = 0; j < BLOCK_DIMENSION; ++j) {
-//            auto original = input(i, j);
-//            auto result = decoded(i, j);
-//            auto difference = std::abs(original - result); // todo they aren't as close as I was expecting...
-//        }
-//    }
-//}
-
-//TEST_CASE("Odd-sized inputs", "") {
-//
-//}
-
+// todo rename to test_matrix and move to include dir
 // Examples from https://matrix.reshish.com/
 TEST_CASE("Matrix Transpose", "") {
     using namespace matrix;
