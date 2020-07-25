998dd111dea9ca865e79af8c3f6510f89f5802ad
Jamie Kirkwin <jkirkwin@gmail.com>
2020-07-25 02:01:45 -0700

    Add compression ratio to testing script

--- uvg/codec.sh
+++ uvg/codec.sh
@@ -7,17 +7,25 @@ make
 
 echo Using quality level $QUALITY
 echo Saving images to $RESULT_DIR
+echo ""
+echo ""
 
 mkdir -p $RESULT_DIR
 rm -f $RESULT_DIR/*
 
 for filename in `find ./test_images -type f`
 do
-    echo Encoding and decoding $filename
-    fname=`basename -s .bmp $filename`
-    echo $fname.bmp
-    tempfile=/tmp/$fname.bin
-    ./uvg_compress $QUALITY $filename $tempfile
-    ./uvg_decompress $tempfile $RESULT_DIR/$fname.bmp
+    echo $filename
+    FNAME=`basename -s .bmp $filename`
+
+    TEMPFILE=/tmp/$FNAME.bin
+    ./uvg_compress $QUALITY $filename $TEMPFILE > /dev/null
+    ./uvg_decompress $TEMPFILE $RESULT_DIR/$FNAME.bmp > /dev/null
+
+    OLDSIZE=`wc $filename | awk '{print $3}'`
+    NEWSIZE=`wc $TEMPFILE | awk '{print $3}'`
+
+    RATIO=$OLDSIZE/$NEWSIZE
+    echo Compressed from $OLDSIZE bytes to $NEWSIZE bytes \($[100*$OLDSIZE/$NEWSIZE ]%\)
     echo "" # Newline
 done
